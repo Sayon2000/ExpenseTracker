ee8d58199ba17cbc3d2c9fcb0ab902d0358d5d96
Sayon Dutta <52020690+Sayon2000@users.noreply.github.com>
2023-11-08 01:13:14 +0530

    frontend for forgot password integration done

--- backend/routes/forgot-password.js
+++ backend/routes/forgot-password.js
@@ -1,14 +1,21 @@
 const express = require('express')
 
 const router = express.Router()
-
 var Brevo = require('@getbrevo/brevo');
 require('dotenv').config()
+
+
+const User = require('../models/user')
 
 
 
 router.post('/forgot-password' , async(req,res)=>{
     try{
+        const email = req.body.email
+        const user = await User.findOne({where : {email : email}})
+        if(user == null)
+             return res.status(404).json({success : true , msg :"Email not found"})
+
         var defaultClient = Brevo.ApiClient.instance;
         var apiKey = defaultClient.authentications['api-key'];
         apiKey.apiKey = process.env.BREVO_API_KEY
@@ -27,12 +34,12 @@ router.post('/forgot-password' , async(req,res)=>{
             subject : 'testing',
             textContent : `click the link to reset your password`
         })
-        return res.json(response)
+        return res.json({success : true})
 
 
     }catch(e){
         console.log(e)
-        return res.json({msg :"Internal server error"})
+        return res.status(500).json({success : false ,msg :"Internal server error"})
     }
 })
 
---
+++ js/forgot-password.js
@@ -0,0 +1,21 @@
+const form = document.forms[0]
+
+form.addEventListener('submit', handleSubmit)
+
+async function handleSubmit(e){
+    e.preventDefault()
+    console.log(e.target.email.value)
+    try{
+        const response = await axios.post('http://localhost:4000/password/forgot-password',{
+            email : e.target.email.value
+        })
+        console.log(response)
+        if(response.status == 200)
+            alert("user not found")
+    }catch(e){
+        console.log(e)
+        if(e.response.status == 404)
+             alert("user not found")
+
+    }
+}
