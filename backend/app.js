b4b4a253d71202bba660db194b42d448e70123f5
Sayon Dutta <52020690+Sayon2000@users.noreply.github.com>
2024-03-05 02:35:12 +0530

    user authentication

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ require('dotenv').config()
 
 
 // const expenseRoutes = require('./routes/expense')
-// const userRoutes = require('./routes/user')
+const userRoutes = require('./routes/user')
 // const paymentsRoutes = require('./routes/purchase')
 // const premiumRoutes = require('./routes/premium')
 
@@ -39,20 +39,9 @@ app.use(compression())
 
 // app.use(morgan('combined',{ stream :accessLogStream}))
 
-// User.hasMany(Expense)
-// Expense.belongsTo(User)
-
-// User.hasMany(Order)
-// Order.belongsTo(User)
-
-// User.hasMany(resetPassword)
-// resetPassword.belongsTo(User)
-
-// User.hasMany(Download)
-// Download.belongsTo(User)
 
 // app.use('/expense' , expenseRoutes)
-// app.use('/user' , userRoutes)
+app.use('/user' , userRoutes)
 // app.use('/payment' , paymentsRoutes)
 // app.use('/premium' , premiumRoutes)
 // app.use('/password', passwordRoutes)
--- backend/controller/user.js
+++ backend/controller/user.js
@@ -4,36 +4,29 @@ const jwt = require('jsonwebtoken')
 
 
 exports.createUser = async (req, res) => {
-    console.log('user creating')
-    let hash = await bcrypt.hash(req.body.password , 10);
-
-    const newUser = new User(req.body.name, req.body.email,hash, false, 0)
-    newUser.save()
-    return res.json({ success: true })
+    try{
 
 
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password;
+    let result = await User.findOne({email : email})
+    // console.log("line 14")
+    // return res.json(result)
+    if(result !== null)
+        return res.status(401).json({success : false , msg : "User already exists"})
+    let hash = await bcrypt.hash(password , 10);
+    const user = await User.create({name : name , email : email , password : hash})
+    
+    console.log(user)
+    const userWithoutPassword = user.toJSON();
+    console.log(userWithoutPassword)
+    delete userWithoutPassword.password;
 
-    // try{
-
-
-    // const name = req.body.name;
-    // const email = req.body.email;
-    // const password = req.body.password;
-    // let result = await User.findOne({where : {email : email}})
-    // console.log(result)
-    // if(result !== null)
-    //     return res.status(401).json({success : false , msg : "User already exists"})
-    // let hash = await bcrypt.hash(password , 10);
-    // const user = await User.create({name : name , email : email , password : hash})
-
-    // const userWithoutPassword = user.toJSON();
-    // console.log(userWithoutPassword)
-    // delete userWithoutPassword.password;
-
-    // return res.json(userWithoutPassword);
-    // }catch(e){
-    //     return res.status(500).json({msg : "Internal server error"})
-    // }
+    return res.json(userWithoutPassword);
+    }catch(e){
+        return res.status(500).json({msg : "Internal server error"})
+    }
 }
 
 
